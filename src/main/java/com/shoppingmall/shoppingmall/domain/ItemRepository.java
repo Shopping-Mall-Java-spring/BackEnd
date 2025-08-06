2cbb07d9224198f9338f28eedb48fb7521815032
NTK0203 <newthink0203@naver.com>
2025-08-06 23:25:09 +0900

    [FEAT] 메인에 상품 목록 반환 (#7)

---
+++ src/main/java/com/shoppingmall/shoppingmall/domain/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.shoppingmall.shoppingmall.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByCategory_CategoryKey(Long categoryKey);
+}
---
+++ src/main/java/com/shoppingmall/shoppingmall/web/ProductController.java
@@ -0,0 +1,31 @@
+package com.shoppingmall.shoppingmall.web;
+
+import com.shoppingmall.shoppingmall.service.ItemService;
+import com.shoppingmall.shoppingmall.web.dto.ProductListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+    private final ItemService itemService;
+
+    //전체 상품 조회
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductListResponseDto>> getAllProducts() {
+        List<ProductListResponseDto> products = itemService.findAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    //특정 카테고리 상품 조회
+    @GetMapping("/categories/{categoryKey}/products")
+    public ResponseEntity<List<ProductListResponseDto>> getProductsByCategory(@PathVariable Long categoryKey) {
+        List<ProductListResponseDto> products = itemService.findProductByCategory(categoryKey);
+        return ResponseEntity.ok(products);
+    }
+}
