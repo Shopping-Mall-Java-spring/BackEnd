9af8f6b2b0f4a9bbbe4a0be795a0f6809a7b4f0b
NTK0203 <newthink0203@naver.com>
2025-08-11 21:38:36 +0900

    [FEAT] 장바구니에서 항목 삭제 (#18)

--- src/main/java/com/shoppingmall/shoppingmall/controller/CartItemController.java
+++ src/main/java/com/shoppingmall/shoppingmall/controller/CartItemController.java
@@ -5,10 +5,7 @@ import com.shoppingmall.shoppingmall.dto.CartItemListResponseDto;
 import com.shoppingmall.shoppingmall.service.CartItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +15,17 @@ import java.util.List;
 public class CartItemController {
     private final CartItemService cartItemService;
 
+    //장바구니 목록 조회
     @GetMapping("/{customerKey}")
     public ResponseEntity<List<CartItemListResponseDto>> getCartItemList(@PathVariable("customerKey") Long customerKey){
         List<CartItemListResponseDto> cartItemList = cartItemService.getCartList(customerKey);
         return ResponseEntity.ok(cartItemList);
+    }
+    //장바구니 삭제
+    @DeleteMapping("/{customerKey}/items/{itemKey}")
+    public ResponseEntity<Void> deleteCartItem(@PathVariable Long customerKey, @PathVariable Long itemKey){
+        cartItemService.deleteCartItem(customerKey, itemKey);
+
+        return ResponseEntity.noContent().build();
     }
 }
